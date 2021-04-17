31b368aa4d819919f18784b74322d9c6f9e6f041
jasonbquick <80070549+jasonbquick@users.noreply.github.com>
2021-04-17 12:28:21 -0400

    Update caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -1,11 +1,4 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-//zebra magazine
-
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'
   function caesar(input, shift, encode = true) {
     if (!shift || shift === 0 || shift < -25 || shift > 25) {
